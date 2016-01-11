7a9715c12b7731f0f4b2185031085a7c650cebdb
Jon Schipp <jschipp@illinois.edu>
2016-01-11 16:36:28 -0600

    virtustotal_lookup.sh: important fixes (e.g. mail spam), refactor code

--- virustotal_lookup.sh
+++ virustotal_lookup.sh
@@ -15,9 +15,15 @@ die(){
   if [ -f ${COWSAY:-none} ]; then
     $COWSAY -d "$*"
   else
-    printf "$*\n"
+    printf "$(date) $*\n" >> virustotal_lookup.log
   fi
-  exit 0
+  exit 1
+}
+
+check_args(){
+  local argc
+  argc="$1"
+  [[ $argc -ge 5 ]] || die "Not enough arguments given"
 }
 
 msg(){
@@ -25,11 +31,11 @@ cat <<EOF
 OSSEC Virus Total Lookup
 - Malware found!
 
-"$results"
+"$RESULTS"
 
-"$alert"
+"$ALERT"
 
-Location: $HOSTNAME $PWD/bin/$SCRIPT
+Location: $HOSTNAME $SCRIPT
 EOF
 }
 
@@ -37,6 +43,53 @@ alert(){
   msg | mail -s "[sa] [${PROG}] $SCRIPT" $MAIL
   msg | $CHAT "$CHANNEL" -
 }
+
+get_alert(){
+  local alert
+  alert=$(awk -v ts=$ALERTID 'BEGIN { RS=""; ORS="\n" } $0 ~ ts { print }' ${PWD}/../logs/alerts/alerts.log)
+  [[ "$alert" ]] || die "No alert found matching timestamp"
+  echo "$alert"
+}
+
+is_hash(){
+  local sha1
+  sha1="$1"
+  [[ $sha1 ]] || "Hash variable empty"
+  [[ $sha1 =~ ^\ +$ ]] && die "Hash variable is only whitespace"
+}
+
+get_hash_from_alert(){
+  local alert
+  local sha1
+  alert="$@"
+  sha1=$(printf "$alert\n" | grep -o '[a-zA-Z0-9]\{40\}' | tail -n 1)
+  is_hash "$sha1"
+  echo "$sha1"
+}
+
+get_hash_from_filename(){
+  local alert
+  local sha1
+  alert="$@"
+  file=$(printf "$alert\n" | awk -F "['']" '/^File|changed for:/ { print $2 }')
+  [[ -r "$file" ]] || die "File not available on system"
+  sha1=$(sha1sum $file | grep -o '[a-zA-Z0-9]\{40\}')
+  is_hash "$sha1"
+  echo "$sha1"
+}
+
+virus_lookup(){
+   local sha1
+   local results
+   sha1="$1"
+   results=$($PWD/bin/virus_total.py $sha1)
+   status_code=$?
+   echo "$results"
+   return $status_code
+}
+
+# Check for arguments
+check_args $#
 
 # Check for chat program
 [[ -f $CHAT ]] || exit 1
@@ -50,13 +103,13 @@ PWD=$(pwd)
 printf "$(date) $0 $1 $2 $3 $4 $5 $6 $7 $8\n" >> ${PWD}/../logs/active-responses.log
 
 # Getting full alert
-alert=$(awk -v ts=$ALERTID 'BEGIN { RS=""; ORS="\n" } $0 ~ ts { print }' ${PWD}/../logs/alerts/alerts.log)
+ALERT=$(get_alert)
 
 # Obtain hash
-sha1=$(printf "$alert\n" | grep -o '[a-zA-Z0-9]\{40\}' | tail -n 1)
-[[ $sha1 ]] || die "No hash found (was a syscheck alert given?)"
+[[ $RULEID -eq 554 ]] && HASH=$(get_hash_from_filename "$ALERT") || HASH=$(get_hash_from_alert "$ALERT")
 
 # Lookup
-results=$($PWD/bin/virus_total.py $sha1)
-[[ "$results" =~ "No entry"|"not malicious" ]] && die "$results"
+RESULTS=$(virus_lookup $HASH) || die "No malware found"
+
+# Notify contacts
 alert
